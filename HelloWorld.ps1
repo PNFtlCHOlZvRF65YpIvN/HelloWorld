073029587a6a6e3d4fe10eae3c8f7e29f42b992a
PNFtlCHOlZvRF65YpIvN <56490824+PNFtlCHOlZvRF65YpIvN@users.noreply.github.com>
2020-01-26 09:51:45 -0500

    Update HelloWorld.ps1
    Updated code to just say "Hello World"

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -2,10 +2,4 @@
 # Modified 2019-10-12 by Bloody
 
 Clear-Host
-Write-Host "Hello World"
-
-# Testing another change
-Write-Host "Second change to see how well source control works on GitHub"
-
-# Testing 3rd change 4th total commit
-Write-Host "This is the 3rd change"
+Write-Output "`n`n    Hello World"
